19140b098e4491fb3827fc7b474bc8dd0f5e79db
IsaacDJohnson <isaac.designws@gmail.com>
2024-01-22 14:02:54 -0600

    completed

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        if not isinstance(value, str):
+            print("The value must be a string.")
+            self._value = ""
+        else:
+            self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, val):
+        if not isinstance(val, str):
+            print("The value must be a string.")
+        else:
+            self._value = val
+
+
+    def is_exclamation(self):
+        return self.value.endswith("!")
+    
+    def is_question(self):
+        return self.value.endswith("?")
+
+    def is_sentence(self):
+        return self.value.endswith(".")
+
+    def count_sentences(self):
+
+        sentences = [s.strip() for s in re.split(r'[.!?]', self.value) if s.strip()]
+        return len(sentences)
+
+pass
